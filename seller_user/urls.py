95861b816328b5ac83c5757c88a10861a5daf7fc
unknown <lamiaah669@gmail.com>
2021-11-07 20:30:34 +0200

    commit

--- seller_user/urls.py
+++ seller_user/urls.py
@@ -4,7 +4,8 @@ app_name = 'seller_user'
 urlpatterns = [
     path('', views.seller ,name='seller_list'),
     path('<int:pk>/seller', views.seller_details, name= 'seller_detail'),
-    path('add_seller/',views.post ,name= 'add_seller'),
+    path('register_seller/',views.post ,name= 'register_seller'),
+    path('add_seller/<int:user_id>',views.post ,name= 'add_seller'),
     path('<int:pk>/delete',views.delete ,name= 'delete'),
     path('edit/<int:pk>/',views.edit,name='edit'),
     ]
