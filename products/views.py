7c11e350e32e005e5b82ea5de1002b70f541eed8
unknown <lamiaah669@gmail.com>
2021-11-08 21:52:40 +0200

    commit

--- products/views.py
+++ products/views.py
@@ -61,8 +61,9 @@ def delete(request ,pk ,seller_id):
         template_name  ='product/delete_pro.html'  
         if request.method == "POST":
             product.is_archive = True
+            seller.id =seller_id
             product.save()
-            return redirect('seller_user:seller_detail',pk =seller)
+            return redirect('seller_user:seller_detail',pk=seller_id)
         context = {'product':product}
         return render(request, template_name, context)  
     else:
