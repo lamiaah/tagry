08885dc5c18e9ad6a06a048a608bf88c966b4dd7
unknown <lamiaah669@gmail.com>
2021-11-10 10:26:34 +0200

    commit

--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from seller_user.models import Seller
 from.forms import  ProductForm ,ImageForm
 from django.views.generic import ListView
 from django.http import HttpResponse
+from seller_user.models import Seller
 from django.contrib.auth.decorators import login_required
 
 
@@ -21,6 +22,7 @@ def get_products(request):
     if request.user.is_authenticated == True:
         product = Products.objects.filter(is_archived=False)
         image = ProductImage.objects.filter(product = product)
+        
         context ={
             'product':product,
             'image':image,
