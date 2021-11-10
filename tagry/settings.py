04d25b18844d92b9b50d1a288e08f3fd98e56d40
unknown <lamiaah669@gmail.com>
2021-11-10 13:40:49 +0200

    commit

--- tagry/settings.py
+++ tagry/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG = False
 
 ALLOWED_HOSTS = ['162.0.236.152','tajiry.com']
 #'127.0.0.1'
