2909896055ab37931b8a27b7549254e46056b2cd
unknown <lamiaah669@gmail.com>
2021-11-13 21:02:57 +0200

    commit

--- products/views.py
+++ products/views.py
@@ -44,10 +44,8 @@ def post_product(request,pk):
                 Productform = ProductForm(request.POST, request.FILES)
                 Imageform = ImageForm(request.POST, request.FILES)
                 if Productform.is_valid() and Imageform.is_valid() :
-                    Productform.instance.created_by =request.user
-                    Productform.instance.updated_by =request.user
-                    Imageform.instance.created_by =request.user
-                    Imageform.instance.updated_by =request.user
+                    Productform.instance.created_user =request.user
+                    Productform.instance.updated_user =request.user
                     Productform.instance.seller_id = seller
                     Productform.save()
                     Imageform.save()
