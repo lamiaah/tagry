295edc38541e32ca2ea795518ae82cb4e2d11193
unknown <lamiaah669@gmail.com>
2021-12-01 14:21:24 +0200

    commit

--- stores/urls.py
+++ stores/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('add_stores<int:pk>/',views.add_stores ,name= 'add_stores'),
 
     path('edit_stores/<int:stores_id>/<int:seller>/', views.edit_stores ,name= 'edit_stores'),
-    path('delete_stores/<int:pk>/<int:seller>/', views.delete ,name= 'delete_stores'),
+    path('delete_stores/<int:pk>/<int:seller>/', views.delete_store ,name= 'delete_stores'),
     
 ]
