a9ede7bbedb698c8037fe1ed6a9de50237cbb098
unknown <lamiaah669@gmail.com>
2021-12-08 15:20:25 +0200

    commit

--- users/Api/views.py
+++ users/Api/views.py
@@ -19,7 +19,7 @@ class UserLogin(APIView):
             if validate:
                 login(request, validate)
                 return Response(
-                    {
+                    {   'id' :request.id,
                         'token' : validate.auth_token.key,
                         'email' : request.data['email']
                     },
