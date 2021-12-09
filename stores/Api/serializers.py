9117f4de2f7c88b3910bfe32d7d8f19fe5b45aa4
unknown <lamiaah669@gmail.com>
2021-12-09 18:08:28 +0200

    commit

--- stores/Api/serializers.py
+++ stores/Api/serializers.py
@@ -7,19 +7,26 @@ from area.models import Area
 
 
 class SellerStoresSerializer(serializers.ModelSerializer):
-
+  store_name =serializers.CharField(read_only=False)
   seller =  serializers.PrimaryKeyRelatedField(queryset = Seller.objects.all())
   store_address =serializers.CharField(read_only=False)
-  store_city =  serializers.PrimaryKeyRelatedField(queryset = Cities.objects.all())
-  store_country =  serializers.PrimaryKeyRelatedField(queryset = Countries.objects.all())
-  store_area =  serializers.PrimaryKeyRelatedField(queryset = Area.objects.all())
+  store_city =  serializers.ReadOnlyField(source='store_city.city_name')
+  store_country =  serializers.ReadOnlyField(source='store_country.country_name')
+  store_area = serializers.ReadOnlyField(source='store_area.area_name')
 
-
-  def create(self, validate_data):
-    return SellerStores.objects.create(**validate_data)  
-
-
+  def create(self, validated_data):
     
+
+    seller = SellerStores(
+      seller_address = validated_data['seller_address'],
+      store_city = Cities.objects.get(pk = validated_data['store_city']),
+      store_country = Countries.objects.get(pk = validated_data['store_country']),
+      store_area = Area.objects.get(pk = validated_data['store_area']),
+    
+    )
+
+    seller.save()
+    return seller
 
   class Meta:
     model = SellerStores
