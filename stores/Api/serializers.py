a6e37edbb84c32767204da3ab139278c0636c3da
unknown <lamiaah669@gmail.com>
2021-12-09 18:22:00 +0200

    commit

--- stores/Api/serializers.py
+++ stores/Api/serializers.py
@@ -8,7 +8,7 @@ from area.models import Area
 
 class SellerStoresSerializer(serializers.ModelSerializer):
   store_name =serializers.CharField(read_only=False)
-  seller =  serializers.PrimaryKeyRelatedField(queryset = Seller.objects.all())
+  # seller =  serializers.PrimaryKeyRelatedField(queryset = Seller.objects.all())
   store_address =serializers.CharField(read_only=False)
   store_city =  serializers.ReadOnlyField(source='store_city.city_name')
   store_country =  serializers.ReadOnlyField(source='store_country.country_name')
