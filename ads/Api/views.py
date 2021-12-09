c9df250563dd3b87581e871a91687e1fbb18ca0a
unknown <lamiaah669@gmail.com>
2021-12-09 14:08:29 +0200

    commit

--- ads/Api/views.py
+++ ads/Api/views.py
@@ -36,3 +36,37 @@ class AdsAdd(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PutAds(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    def get_object(self, id):
+        try:
+            return Ads.objects.get(id=id)
+        except Ads.DoesNotExist:
+            return Http404
+
+
+    def get(self,request ,id):
+
+        Ads = self.get_object(id)
+        serializer = AdsSerializer(Ads , context = {"request":request}) 
+        return Response(serializer.data)
+
+
+    def put(self ,request ,id ):
+        Ads = self.get_object(id)
+        serializer = AdsSerializer(Ads ,data=request.data , context = {"request":request}) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors ,status= status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request ,id):
+        Ads= self.get_object(id)
+        if Ads == Http404:
+            return Response(status= status.HTTP_400_BAD_REQUEST)
+        else:
+            Ads.is_archived = True
+            Ads.save()
+            return Response(status= status.HTTP_200_OK) 
