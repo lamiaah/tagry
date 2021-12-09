8633ee2278cca9f79dafa38655e5585b5bfcb847
unknown <lamiaah669@gmail.com>
2021-12-09 13:16:15 +0200

    commit

--- area/Api/serializers.py
+++ area/Api/serializers.py
@@ -3,11 +3,12 @@ from rest_framework import serializers
 from area.models import Area
 
 class AreaSerializer(serializers.ModelSerializer):
-    
+    city_name =  serializers.ReadOnlyField(source='city_name.city_name')
+    country_name =  serializers.ReadOnlyField(source='country_name.country_name')
     area_name = serializers.CharField(read_only=True)
     id = serializers.PrimaryKeyRelatedField(read_only = True)
-    country_name = serializers.RelatedField(read_only = True)
-    city_name = serializers.RaletedField(read_only = True)
+    # country_name = serializers.RelatedField(read_only = True)
+    # city_name = serializers.RaletedField(read_only = True)
     
     
     class Meta:
