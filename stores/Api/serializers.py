cd589435c814fc735aed9332a284533bc8273ced
unknown <lamiaah669@gmail.com>
2021-12-09 15:29:00 +0200

    commit

--- stores/Api/serializers.py
+++ stores/Api/serializers.py
@@ -1,15 +1,18 @@
 from stores.models import SellerStores
 from rest_framework import serializers
-from products.models import Products
-from users.models import CustomUser
+from seller_user.models import Seller
+from cities.models import Cities
+from countries.models import Countries
+from area.models import Area
 
 
 class SellerStoresSerializer(serializers.ModelSerializer):
-  seller =  serializers.ReadOnlyField(source='seller_id.id')
+
+  seller =  serializers.PrimaryKeyRelatedField(queryset = Seller.objects.all())
   store_address =serializers.CharField(read_only=False)
-  store_city =  serializers.ReadOnlyField(source='store_city.city_name')
-  store_country =  serializers.ReadOnlyField(source='store_country.country_name')
-  store_area =  serializers.ReadOnlyField(source='store_area.area_name')
+  store_city =  serializers.PrimaryKeyRelatedField(queryset = Cities.objects.all())
+  store_country =  serializers.PrimaryKeyRelatedField(queryset = Countries.objects.all())
+  store_area =  serializers.PrimaryKeyRelatedField(queryset = Area.objects.all())
 
 
   def create(self, validate_data):
