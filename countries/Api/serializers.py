6d4f37dec93c3ce8f44a0a54aceb457704dcebf2
unknown <lamiaah669@gmail.com>
2021-12-11 20:52:20 +0200

    commit

--- countries/Api/serializers.py
+++ countries/Api/serializers.py
@@ -9,7 +9,8 @@ class CountriesSerailizer(serializers.ModleSerializer):
     country_lanuage_code = serializers.RelatedField(read_only =True)
     phone_code = serializers.CharField(read_only = True)
 
-
+    def create(self, validate_data):
+       return Countries.objects.create(**validate_data)  
     class Meta:
         model=Countries
         fields=['id','user_id','code','country_name','country_language_code','phone_code']
