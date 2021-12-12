a2caf76d587bebc17ba7d3262929be70926d8d07
unknown <lamiaah669@gmail.com>
2021-12-12 10:43:09 +0200

    commit

--- countries/Api/views.py
+++ countries/Api/views.py
@@ -8,21 +8,21 @@ from area.Api.serializers import AreaSerializer
 from countries.Api.serializers import CountriesSerailizer
 from cities.Api.serializers import CitiesSerailizer
 
-class CountriesApiList(APIView):
-    def get(self,requset):
+# class CountriesApiList(APIView):
+#     def get(self,requset):
         
-       try:
-           countries =Countries.objects.all()
-           country_serializer = CountriesSerailizer(countries, many=True)
-           for country in country_serializer.data:
-               cities = Cities.objects.filter(country_name=country['id'])
-               cities_serializer = CitiesSerailizer(cities,many=True)
-               country['cities'] = cities_serializer.data
-               for city in cities_serializer.data:
-                   areas = Area.objects.filter(city_name=city['id'])
-                   area_serializer = AreaSerializer(areas,many= True)
-                   city['areas'] = area_serializer.data
-           return Response(country_serializer.data ,status=status.HTTP_200_OK)
-       except Exception as e :
-            return Response({'error':'some thing went wrong'},status= status.HTTP_400_BAD_REQUEST)
+#        try:
+#            countries =Countries.objects.all()
+#            country_serializer = CountriesSerailizer(countries, many=True)
+#            for country in country_serializer.data:
+#                cities = Cities.objects.filter(country_name=country['id'])
+#                cities_serializer = CitiesSerailizer(cities,many=True)
+#                country['cities'] = cities_serializer.data
+#                for city in cities_serializer.data:
+#                    areas = Area.objects.filter(city_name=city['id'])
+#                    area_serializer = AreaSerializer(areas,many= True)
+#                    city['areas'] = area_serializer.data
+#            return Response(country_serializer.data ,status=status.HTTP_200_OK)
+#        except Exception as e :
+#             return Response({'error':'some thing went wrong'},status= status.HTTP_400_BAD_REQUEST)
 
