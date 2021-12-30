aba51146eb09acdd188fb0f89907bb7e8fe46b62
unknown <lamiaah669@gmail.com>
2021-12-30 11:21:57 +0200

    commit

--- users/Api/urls.py
+++ users/Api/urls.py
@@ -1,13 +1,13 @@
 
 from django.urls import path
-from users.Api.views import UserLogin, UserLogout, UserData, UpdateUserPassword ,RegisterView
+from users.Api.views import UserLogin, UserLogout, UserData, UpdateUserPassword ,RegisterAPI
 
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('userLogin', UserLogin.as_view()),
-    path('reister/',RegisterView.as_view()),
+    path('reister/',RegisterAPI.as_view()),
     path('userLogout', UserLogout.as_view()),
     path('userData/<int:pk>', UserData.as_view()),
     path('updatePassword/<int:pk>', UpdateUserPassword.as_view())
