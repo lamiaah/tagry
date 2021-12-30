22dc73d239c53b806bbeb3a69f70f09dca8d2ab2
unknown <lamiaah669@gmail.com>
2021-12-30 20:11:32 +0200

    commit

--- tagry/settings.py
+++ tagry/settings.py
@@ -87,9 +87,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         
     ]  
 }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
