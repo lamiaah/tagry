8309fd664810e9ffa99b06d19e30191b4418eefe
unknown <lamiaah669@gmail.com>
2021-12-30 20:31:29 +0200

    commit

--- users/Api/serializers.py
+++ users/Api/serializers.py
@@ -16,7 +16,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
+    def save(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
 
         return user
