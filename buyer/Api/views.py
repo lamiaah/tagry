e1d14d0663880111b54d0cd4546f22b4559ab457
unknown <lamiaah669@gmail.com>
2021-12-31 15:32:11 +0200

    commit

--- buyer/Api/views.py
+++ buyer/Api/views.py
@@ -9,46 +9,50 @@ from buyer.Api.serializer import BuyerSerializer
 class BuyerLogin(APIView):
 
     def post(self, request):
-        try:
-            email = request.data.get('email')
-            password = request.data.get('password')
-            validate = authenticate(email= email, password = password)
+    
+        email = request.data.get('email')
+        password = request.data.get('password')
+        validate = authenticate(email= email, password = password)
 
-            if validate:
-                login(request, validate)
-                us = CustomUser.objects.get(email = email)
-                data =  {'id': us.id,
-                        'token' : validate.auth_token.key,
-                        'email' : request.data['email']
-                    }
-
-                return Response(
-                 data, status.HTTP_200_OK
-                )
-            else:
-                return Response('Invalid Login', status = status.HTTP_401_UNAUTHORIZED)
-        except Exception as e:
-            return Response(str(e), status = status.HTTP_400_BAD_REQUEST)
-
-class BuyerInfo(APIView):
+        if validate:
+            login(request, validate)
+            us = CustomUser.objects.get(email = email)
+            data =  {'id': us.id,
+                    'token' : validate.auth_token.key,
+                    'email' : request.data['email']
+                }
+            x=  data['id']
+            if request.method == 'POST':
+                user = CustomUser.objects.filter(id= x)
+                serializer =BuyerSerializer(data = request.data ,many= True)
+                request.data["user_id"] = user.id
+                if serializer.is_valid():
+                
+                    serializer.save()  
+                    return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)      
+                else:
+                
+                    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST) 
+        
+# class BuyerInfo(APIView):
   
   
-    def post(self, request):
-        # try:
-        #     user = CustomUser.objects.filter(id=user_id)            
-        # except CustomUser.DoesNotExist:
-        #     return Response(status=status.HTTP_404_NOT_FOUND) 
+#     def post(self, request):
+#         # try:
+#         #     user = CustomUser.objects.filter(id=user_id)            
+#         # except CustomUser.DoesNotExist:
+#         #     return Response(status=status.HTTP_404_NOT_FOUND) 
 
-        if request.method == 'POST':
+#         if request.method == 'POST':
             
-            serializer =BuyerSerializer(data = request.data ,many= True)
-            request.data["user_id"] = request.user.id
-            if serializer.is_valid():
+#             serializer =BuyerSerializer(data = request.data ,many= True)
+#             request.data["user_id"] = request.user.id
+#             if serializer.is_valid():
             
-                serializer.save()  
-                return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)      
-            else:
+#                 serializer.save()  
+#                 return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)      
+#             else:
               
-                return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST) 
+#                 return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST) 
         
        
