bcdf8dca82776253e9a7709f2eab6c592495777f
unknown <lamiaah669@gmail.com>
2021-12-31 21:25:51 +0200

    commit

--- buyer/Api/views.py
+++ buyer/Api/views.py
@@ -54,7 +54,7 @@ class BuyerInfo(APIView):
             if serializer.is_valid():
             
                 serializer.save()  
-                return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)      
+                return Response(serializer.data, status = status.HTTP_200_OK)      
             else:
               
                 return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST) 
