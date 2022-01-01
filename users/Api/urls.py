cc7ad96b7500a4651a4f285b0dba8220ff8f9177
unknown <lamiaah669@gmail.com>
2022-01-01 23:57:53 +0200

    commit

--- users/Api/urls.py
+++ users/Api/urls.py
@@ -1,13 +1,13 @@
 
 from django.urls import path
-from users.Api.views import  UserLogout,UpdateUserPassword 
+from users.Api.views import  UserrLogout,UpdateUserPassword 
 
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
   
-    path('userLogout', UserLogout.as_view()),
+    path('userLogout', UserrLogout.as_view()),
     
     path('updatePassword/<int:user_id>', UpdateUserPassword.as_view())
 ]
--- users/Api/views.py
+++ users/Api/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import GenericAPIView
 from django.views.decorators.csrf import csrf_exempt
 from users.Api.api_register import validate_token
-
+from users.Api.serializers import  CustomUserSerializer
 
 
 def login_user(request):
@@ -42,104 +42,104 @@ def login_user(request):
 
 
 
-class UserLogout(APIView):
-
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, user_id):
-        try:
-            token_validation = validate_token(request, user_id)
-            if token_validation == True:
-                logout(request)
-                # request.auth.delete()
-                return Response('Success', status.HTTP_200_OK)
-            else:
-                return Response('invalid data', status = status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({'error' : str(e)}, status.HTTP_406_NOT_ACCEPTABLE)
-
-
-
-
-
-class UpdateUserPassword(APIView):
-
-    permission_classes = [IsAuthenticated]
-
-
-    def put(self, request, user_id):
-
-        try:
-            try:
-                user_data = CustomUser.objects.get(pk = user_id)
-            except CustomUser.DoesNotExist:
-                return Response(
-                    {
-                        'error' : 'no user found',
-                    },
-                    status.HTTP_404_NOT_FOUND
-                )
-            else:
-                token_validation = validate_token(request)
-                if token_validation == True:
-                    user_data.set_password(request.data.get('new_password'))
-                    user_data.save()
-                    return Response(
-                        {
-                            'response' : 'password updated success',
-                        },
-                        status.HTTP_200_OK
-                    )
-                else:
-                    return Response(
-                        {
-                            'error' : 'invalid data',
-                        },
-                        status.HTTP_406_NOT_ACCEPTABLE
-                    )
-
-        except Exception as e:
-            return Response(
-                {
-                    'error' : 'some thing went wrong {}'.format(e),
-                },
-                status.HTTP_400_BAD_REQUEST
-            )
-
-
-
-
-# class UserData(APIView):
-
-#     permission_classes = [IsAuthenticated]
-
-#     def get(self, request, pk):
-
-#         try:
-#             user_data = CustomUser.objects.get(pk = pk)
-#             serializer = UserSerializer(user_data)
-#             return Response(serializer.data, status.HTTP_200_OK)
-#         except CustomUser.DoesNotExist:
-#             return Response(
-#                 {
-#                     'error' : 'no user found',
-#                 },
-#                 status.HTTP_404_NOT_FOUND
-#             )
-
-
-
 # class UserLogout(APIView):
 
 #     permission_classes = [IsAuthenticated]
 
-#     def post(self, request):
+#     def post(self, request, user_id):
 #         try:
-#             logout(request)
-#             request.auth.delete()
-#             return Response('Success', status.HTTP_200_OK)
+#             token_validation = validate_token(request, user_id)
+#             if token_validation == True:
+#                 logout(request)
+#                 # request.auth.delete()
+#                 return Response('Success', status.HTTP_200_OK)
+#             else:
+#                 return Response('invalid data', status = status.HTTP_400_BAD_REQUEST)
 #         except Exception as e:
 #             return Response({'error' : str(e)}, status.HTTP_406_NOT_ACCEPTABLE)
+
+
+
+
+
+# class UpdateUserPassword(APIView):
+
+#     permission_classes = [IsAuthenticated]
+
+
+#     def put(self, request, user_id):
+
+#         try:
+#             try:
+#                 user_data = CustomUser.objects.get(pk = user_id)
+#             except CustomUser.DoesNotExist:
+#                 return Response(
+#                     {
+#                         'error' : 'no user found',
+#                     },
+#                     status.HTTP_404_NOT_FOUND
+#                 )
+#             else:
+#                 token_validation = validate_token(request)
+#                 if token_validation == True:
+#                     user_data.set_password(request.data.get('new_password'))
+#                     user_data.save()
+#                     return Response(
+#                         {
+#                             'response' : 'password updated success',
+#                         },
+#                         status.HTTP_200_OK
+#                     )
+#                 else:
+#                     return Response(
+#                         {
+#                             'error' : 'invalid data',
+#                         },
+#                         status.HTTP_406_NOT_ACCEPTABLE
+#                     )
+
+#         except Exception as e:
+#             return Response(
+#                 {
+#                     'error' : 'some thing went wrong {}'.format(e),
+#                 },
+#                 status.HTTP_400_BAD_REQUEST
+#             )
+
+
+
+
+class UserData(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+
+        try:
+            user_data = CustomUser.objects.get(pk = pk)
+            serializer =  CustomUserSerializer(user_data)
+            return Response(serializer.data, status.HTTP_200_OK)
+        except CustomUser.DoesNotExist:
+            return Response(
+                {
+                    'error' : 'no user found',
+                },
+                status.HTTP_404_NOT_FOUND
+            )
+
+
+
+class UserrLogout(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            logout(request)
+            request.auth.delete()
+            return Response('Success', status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error' : str(e)}, status.HTTP_406_NOT_ACCEPTABLE)
 
 
 
