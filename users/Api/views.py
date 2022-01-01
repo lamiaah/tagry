ba2266d50269490c4cf9a61f821270533ada34ce
unknown <lamiaah669@gmail.com>
2022-01-01 20:26:10 +0200

    commit

--- users/Api/views.py
+++ users/Api/views.py
@@ -1,5 +1,5 @@
 from datetime import date
-from users.Api.serializers import RegistrationSerializer
+
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from rest_framework.views import APIView
